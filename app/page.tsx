f1e9b600727402b6742249d84eaabd8c1c2e1d70
Yongho Kim <drata313@gmail.com>
2024-07-25 22:26:23 +0900

    feat: JIT

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,15 @@ export default function Home() {
   return (
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
       <div className="bg-white w-full max-w-screen-sm  shadow-lg p-5 rounded-2xl flex flex-col gap-5">
-        {["H", "ME", "YOU", "YOURSELF"].map((person, index) => (
-          <div
-            key={index}
-            className="flex items-center gap-5 border-b-2 pb-5 last:pb-0 last:border-0 "
-          >
-            <div className="size-10 bg-blue-400 rounded-full" />
-
-            <span>{person}</span>
-            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
-              <span className="z-10">{index}</span>
-              <div className="size-6 bg-red-500 rounded-full absolute animate-ping"></div>
+        {["H", "ME", "YOU", "YOURSELF", ""].map((person, index) => (
+          <div key={index} className="flex items-center gap-5 group">
+            <div className="group flex flex-col">
+              <input
+                className="bg-gray-200 w-full"
+                placeholder="write your email"
+              />
+              <span className="group-focus-within:block hidden">에러!!</span>
+              <button>submit</button>
             </div>
           </div>
         ))}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,19 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  // tell to TailwindCSS's JIT (Just-In-Time) what paths exist
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      borderRadius: {
+        "sex-on-the-beach": "69px",
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
