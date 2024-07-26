7a59d8396a5b8d2aa32d68f405e6206b8e512dbe
Yongho Kim <drata313@gmail.com>
2024-07-26 21:08:28 +0900

    feat: zod refinement

--- actions/create-account/index.ts
+++ actions/create-account/index.ts
@@ -2,12 +2,41 @@
 
 import { z } from "zod";
 
-const schema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-});
+const checkUsername = (username: string) => {
+  return !username.includes("김용호");
+};
+
+const checkPasswordMatch = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => {
+  return password === confirm_password;
+};
+
+const schema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "닉네임에 수상한(?) 문자가 섞인 것 같아요.",
+        required_error: "닉네임을 작성해주세요.",
+      })
+      .min(3, "닉네임은 3글자 이상 작성해주세요.")
+      .max(10, "닉네임은 10글자 이하로 작성해주세요.")
+      .refine(
+        checkUsername,
+        "김용호라는 단어는 어디서 아셨죠? 이 로그는 저장됐어요."
+      ),
+    email: z.string().email("이메일 주소가 유효하지 않습니다."),
+    password: z.string().min(10, "비밀번호는 10글자 이상 작성해주세요."),
+    confirm_password: z.string().min(10),
+  })
+  .refine(checkPasswordMatch, {
+    message: "비밀번호가 일치하지 않아요.",
+    path: ["confirm_password"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -18,6 +47,7 @@ export async function createAccount(prevState: any, formData: FormData) {
   };
   const result = schema.safeParse(data);
   if (!result.success) {
+    console.log(result.error.flatten());
     return result.error.flatten();
   }
 }
