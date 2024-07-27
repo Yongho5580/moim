6c52db1b983383553fd66220bd00aabca66a70e2
Yongho Kim <drata313@gmail.com>
2024-07-27 15:50:22 +0900

    feat: SMS Validation

--- actions/sms/index.ts
+++ actions/sms/index.ts
@@ -2,9 +2,39 @@
 
 import { z } from "zod";
 import validator from "validator";
+import { redirect } from "next/navigation";
+import { PHONE_MESSAGES } from "@/constants/messages";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    PHONE_MESSAGES["INVALID_TYPE"]
+  );
 
 const verifySchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsVerification(prevState: any, formData: FormData) {}
+interface ActionState {
+  verify: boolean;
+}
+
+export async function smsVerification(
+  prevState: ActionState,
+  formData: FormData
+) {
+  const phone = formData.get("phone");
+  const verify = formData.get("verify");
+
+  if (!prevState.verify) {
+    const result = phoneSchema.safeParse(phone);
+    return !result.success
+      ? { verify: false, error: result.error.flatten() }
+      : { verify: true };
+  } else {
+    const result = verifySchema.safeParse(verify);
+    return !result.success
+      ? { verify: true, error: result.error.flatten() }
+      : redirect("/");
+  }
+}
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -5,8 +5,13 @@ import Button from "@/components/button";
 import Input from "@/components/input";
 import { useFormState } from "react-dom";
 
+const initialState = {
+  verify: false,
+  error: undefined,
+};
+
 export default function SMSLogin() {
-  const [state, formAction] = useFormState(smsVerification, null);
+  const [state, formAction] = useFormState(smsVerification, initialState);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -14,16 +19,27 @@ export default function SMSLogin() {
         <h2 className="text-xl">모임에 참여하기 위한 과정이에요!</h2>
       </div>
       <form action={formAction} className="flex flex-col gap-3">
-        <Input name="phone" type="text" placeholder="휴대폰 번호" required />
-        <Input
-          name="verify"
-          type="text"
-          placeholder="인증 번호"
-          required
-          min={100000}
-          max={999999}
-        />
-        <Button text="인증하기" />
+        {state.verify ? (
+          <Input
+            key="verify"
+            name="verify"
+            type="text"
+            placeholder="인증 번호"
+            required
+            min={100000}
+            max={999999}
+          />
+        ) : (
+          <Input
+            key="phone"
+            name="phone"
+            type="tel"
+            placeholder="휴대폰 번호"
+            required
+            errors={state.error?.formErrors}
+          />
+        )}
+        <Button text={state.verify ? "확인" : "문자로 인증하기"} />
       </form>
     </div>
   );
