0ac71de73f53706ad794d4c635a365cfc0b6b18d
Yongho Kim <drata313@gmail.com>
2024-08-02 01:06:35 +0900

    feat: add load more logic

--- components/products/ProductList.tsx
+++ components/products/ProductList.tsx
@@ -12,12 +12,17 @@ export default function ProductList({ initialProducts }: IProductListProps) {
   const [products, setProducts] = useState(initialProducts);
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(0);
+  const [isLastPage, setIsLastPage] = useState(false);
 
   const handleLoadMoreClick = async () => {
     setIsLoading(true);
     const newProducts = await getMoreProducts(page + 1);
-    setPage((prev) => prev + 1);
-    setProducts((prev) => [...prev, ...newProducts]);
+    if (newProducts.length !== 0) {
+      setPage((prev) => prev + 1);
+      setProducts((prev) => [...prev, ...newProducts]);
+    } else {
+      setIsLastPage(true);
+    }
     setIsLoading(false);
   };
   return (
@@ -25,13 +30,15 @@ export default function ProductList({ initialProducts }: IProductListProps) {
       {products.map((product) => (
         <ProductItem key={product.id} {...product} />
       ))}
-      <button
-        onClick={handleLoadMoreClick}
-        disabled={isLoading}
-        className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
-      >
-        {isLoading ? "로딩 중" : "더보기"}
-      </button>
+      {!isLastPage && (
+        <button
+          onClick={handleLoadMoreClick}
+          disabled={isLoading}
+          className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+        >
+          {isLoading ? "로딩 중" : "더보기"}
+        </button>
+      )}
     </div>
   );
 }
