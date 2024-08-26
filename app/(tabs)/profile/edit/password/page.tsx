b4e2b09ad58236f837b82004d60d70cdee6931e9
Yongho Kim <drata313@gmail.com>
2024-08-26 23:01:01 +0900

    feat: add password page metadata

--- app/(tabs)/profile/edit/password/page.tsx
+++ app/(tabs)/profile/edit/password/page.tsx
@@ -1,43 +1,10 @@
-"use client";
+import { EditPasswordForm } from "@/components/profile/EditPasswordForm";
+import { Metadata } from "next";
 
-import { updatePassword } from "@/actions/profile/edit/password";
-import { SubmitButton } from "@/components/common/SubmitButton";
-import { InputWithError } from "@/components/common/InputWithError";
-import { Label } from "@/components/ui/label";
-import { useFormState } from "react-dom";
+export const metadata: Metadata = {
+  title: "비밀번호 변경",
+};
 
 export default function EditPassword() {
-  const [state, formAction] = useFormState(updatePassword, null);
-  return (
-    <form action={formAction} className="p-5 flex flex-col gap-5">
-      <div className="flex flex-col gap-1.5">
-        <Label htmlFor="currentPassword">현재 비밀번호</Label>
-        <InputWithError
-          name="currentPassword"
-          placeholder="현재 비밀번호를 입력하세요."
-          type="password"
-          errors={state?.fieldErrors.currentPassword}
-        />
-      </div>
-      <div className="flex flex-col gap-1.5">
-        <Label htmlFor="newPassword">새 비밀번호</Label>
-        <InputWithError
-          name="newPassword"
-          placeholder="새 비밀번호를 입력하세요."
-          type="password"
-          errors={state?.fieldErrors.newPassword}
-        />
-      </div>
-      <div className="flex flex-col gap-1.5">
-        <Label htmlFor="checkNewPassword">새 비밀번호 확인</Label>
-        <InputWithError
-          name="checkNewPassword"
-          placeholder="새 비밀번호를 확인하세요."
-          type="password"
-          errors={state?.fieldErrors.checkNewPassword}
-        />
-      </div>
-      <SubmitButton>확인</SubmitButton>
-    </form>
-  );
+  return <EditPasswordForm />;
 }
