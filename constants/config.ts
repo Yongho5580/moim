5d3302ddd78962b21b3c70232bf5bc6eebb6ed7f
Yongho Kim <drata313@gmail.com>
2024-09-04 12:12:40 +0900

    feat: use environment variable for Supabase key

--- constants/config.ts
+++ constants/config.ts
@@ -3,6 +3,5 @@ export const AWS_REGION = process.env.AWS_REGION;
 export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 export const SUPABASE_URL = "https://urmbkhwdugjseqnjpnsh.supabase.co";
-export const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVybWJraHdkdWdqc2VxbmpwbnNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMwMTQzNTMsImV4cCI6MjAzODU5MDM1M30.P6m_CJVMkmQ4ZSD4qadp-lgrDTb7nkHJSpfJ1ItVfD4";
+export const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 export const AWS_S3_BASE_URL = "https://moim-s3.s3.us-east-2.amazonaws.com";
